980522be212a7e69f586cba6ef58212802a16451
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-11-24 19:40:02 +0530

    update post and add cards

--- src/compo/Post.jsx
+++ src/compo/Post.jsx
@@ -1,15 +1,12 @@
-const Post = () => {
+const Post = ({ post }) => {
   return (
-    <div className="card" style={{ width: "18rem" }}>
+    <div className="card post-card" style={{ width: "18rem" }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        <h5 className="card-title">{post.title}</h5>
+        <p className="card-text">{post.content}</p>
+        {post.tags.map((tag) => (
+          <span class="badge text-bg-primary ">{tag}</span>
+        ))}
       </div>
     </div>
   );
--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,8 +1,56 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-const PostList = createContext({});
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  return currPostList; // Placeholder
+};
 
 const PostListProvider = ({ children }) => {
-  return <PostList.Provider value={[]}>{children}</PostList.Provider>;
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = () => {};
+
+  const deletePost = () => {};
+
+  return (
+    <PostList.Provider
+      value={{
+        postList,
+        addPost,
+        deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
 };
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "First Post",
+    content: "This is the content of the first post.",
+    userID: "user1",
+    tags: ["welcome", "first", "introduction"],
+    reactions: { like: 10, love: 5, laugh: 2 },
+  },
+
+  {
+    id: "2",
+    title: "Second Post",
+    content: "This is the content of the second post.",
+    userID: "user2",
+    tags: ["update", "news", "second"],
+    reactions: { like: 7, love: 3, laugh: 1 },
+  },
+];
+
 export default PostListProvider;
