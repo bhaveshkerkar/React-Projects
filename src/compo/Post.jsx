59d88c4be91997678812847aeb771ad70bd1169a
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-11-24 21:25:08 +0530

    add tags

--- src/compo/Post.jsx
+++ src/compo/Post.jsx
@@ -3,9 +3,15 @@ const Post = ({ post }) => {
     <div className="card post-card" style={{ width: "18rem" }}>
       <div className="card-body">
         <h5 className="card-title">{post.title}</h5>
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          {post.reactions.like}
+          <span class="visually-hidden">unread messages</span>
+        </span>
         <p className="card-text">{post.content}</p>
         {post.tags.map((tag) => (
-          <span class="badge text-bg-primary ">{tag}</span>
+          <span key={tag} className="badge text-bg-primary m-1">
+            #{tag}
+          </span>
         ))}
       </div>
     </div>
