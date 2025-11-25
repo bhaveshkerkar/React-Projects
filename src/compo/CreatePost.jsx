9b118089e46eb3c985babf3c67d45f58ad319ff7
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-11-26 00:00:03 +0530

    remaing files added

--- src/compo/CreatePost.jsx
+++ src/compo/CreatePost.jsx
@@ -1,40 +1,88 @@
+import React, { useRef } from "react";
+import { useContext } from "react";
+import { PostList } from "../store/post-list-store";
+
 const CreatePost = () => {
+  const { addPost } = useContext(PostList);
+
+  const userId = useRef();
+  const postTitle = useRef();
+  const postBody = useRef();
+  const postReactions = useRef();
+  const postTags = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form className="create-post" onSubmit={handleSubmit}>
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+        <label htmlFor="userId" className="form-label">
+          Enter your User ID here !
         </label>
         <input
-          type="email"
+          type="text"
+          ref={userId}
           className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
+          id="userId"
+          placeholder="Your user id"
         />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
       </div>
+
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
+        <label htmlFor="title" className="form-label">
+          Post Title
         </label>
         <input
-          type="password"
+          type="text"
+          ref={postTitle}
           className="form-control"
-          id="exampleInputPassword1"
+          id="title"
+          placeholder="How are you feeling today!"
         />
       </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          Check me out
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post content
         </label>
+        <textarea
+          type="text"
+          ref={postBody}
+          rows="4"
+          className="form-control"
+          id="body"
+          placeholder="What's on your mind?"
+        />
       </div>
+
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Enter Number of Reactions
+        </label>
+        <textarea
+          type="text"
+          ref={postReactions}
+          className="form-control"
+          id="reactions"
+          placeholder="How many reactions do you expect?"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter Your hashtags here!
+        </label>
+        <textarea
+          type="text"
+          ref={postTags}
+          className="form-control"
+          id="tags"
+          placeholder="Enter tags separated by commas"
+        />
+      </div>
+
       <button type="submit" className="btn btn-primary">
         Submit
       </button>
