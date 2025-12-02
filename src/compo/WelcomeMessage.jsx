ddc67133834ca9b8c8edec3954bd65fcbc884474
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-02 23:17:12 +0530

    edit page

--- src/compo/WelcomeMessage.jsx
+++ src/compo/WelcomeMessage.jsx
@@ -1,5 +1,9 @@
-const WelcomeMessage = () => {
-  return <h1>There are no Posts</h1>;
+const WelcomeMessage = ({ onGetPostsClick }) => {
+  return (
+    <center className="welcome-message">
+      <h1>there are no posts </h1>
+    </center>
+  );
 };
 
 export default WelcomeMessage;
